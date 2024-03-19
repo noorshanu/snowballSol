5809635c28b4ccc432cd558a9bdb904aeb4d1db6
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-19 16:56:37 +0530

    new

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
              
             </div>
             <div className="flex items-center space-x-4 text-xl [&_a:hover]:opacity-70">
-              <a target="_blank" href="https://twitter.com/DinoDogee">
+              <a target="_blank" href="https://twitter.com/SnowballSol">
                 <FaTwitter fill="#4D2E7B" />
               </a>
               <a target="_blank" href="https://t.me/dinodogeso">
